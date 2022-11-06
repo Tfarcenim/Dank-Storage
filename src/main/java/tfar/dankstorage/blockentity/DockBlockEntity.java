8531840b657cf4cca6ef56008203a616bbaa8a3b
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2022-11-06 04:59:42 -0500

    Update DockBlockEntity.java

--- src/main/java/tfar/dankstorage/blockentity/DockBlockEntity.java
+++ src/main/java/tfar/dankstorage/blockentity/DockBlockEntity.java
@@ -15,8 +15,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import tfar.dankstorage.DankStorage;
@@ -250,7 +250,7 @@ public class DockBlockEntity extends BlockEntity implements Nameable, MenuProvid
 
     @Override
     public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
-        return cap == ForgeCapabilities.ITEM_HANDLER ? optional.cast() : super.getCapability(cap, side);
+        return cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY ? optional.cast() : super.getCapability(cap, side);
     }
 
     @Override
